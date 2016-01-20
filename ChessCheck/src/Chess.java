8813afcbdc228ca04779feced125cf6bfa9198e5
Varrenhunter <ethan.farthing.13@cnu.edu>
2016-01-20 15:00:28 -0500

    This is code and notes for CPSC 280.
    Code and notes for CPSC 280.

---
+++ ChessCheck/src/Chess.java
@@ -0,0 +1,29 @@
+
+public class Chess {
+
+	private boolean hasBlackKing(char spot){
+		if(spot == 'k'){
+			return true;
+		}
+		return false;
+	}
+	private boolean hasWhiteKing(char spot){
+		if(spot == 'K'){
+			return true;
+		}
+		return false;
+	}
+	private boolean blackPawnChecksKing(char[][] board){
+		for(int i = 0; i<)
+	}
+	private boolean whitePawnChecksKing(char[][] board){
+		
+	}
+	private boolean black
+	
+	
+	public static char getCheck(char[][] board){
+		return '0';
+	}
+	
+}
